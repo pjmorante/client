666209fed3872297a2d8d54203f547698b4d595e
Pedro Morante <pedro.morante@outlook.com>
2024-11-14 22:54:35 -0500

    add ProductDetail component

--- src/views/Products.tsx
+++ src/views/Products.tsx
@@ -1,12 +1,17 @@
-import { Link } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import { getProducts } from "../services/ProductService";
+import ProductDetail from "../components/ProductDetail";
+import { Product } from "../types";
 
 export async function loader() {
-  await getProducts()
-  return {}
+  const products = await getProducts()
+  return products;
 }
 
 export default function Products() {
+
+  const products = useLoaderData() as Product[];
+
   return (
     <>
       <div className="flex justify-between">
@@ -18,6 +23,27 @@ export default function Products() {
         >
           Agregar Producto
         </Link>
+      </div>
+
+      <div className="p-2">
+        <table className="w-full mt-5 table-auto">
+          <thead className="bg-slate-800 text-white">
+            <tr>
+              <th className="p-2">Producto</th>
+              <th className="p-2">Precio</th>
+              <th className="p-2">Disponibilidad</th>
+              <th className="p-2">Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products.map(product => (
+              <ProductDetail 
+                key={product.id}
+                product={product}
+              />
+            ))}
+          </tbody>
+        </table>
       </div>
     </>
   );
