1dcdce64e9bdcddc9881fc056fae21db8554a775
Pedro Morante <pedro.morante@outlook.com>
2024-11-15 22:06:27 -0500

    add EditProduct component

--- src/services/ProductService.ts
+++ src/services/ProductService.ts
@@ -1,5 +1,5 @@
 import { safeParse } from "valibot";
-import { DraftProductSchema, ProductsSchema } from "../types";
+import { DraftProductSchema, Product, ProductSchema, ProductsSchema } from "../types";
 import axios from "axios";
 
 type ProductData = {
@@ -37,6 +37,21 @@ export async function getProducts() {
     }else{
       throw new Error("Hubo un error... :(");
       
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getProductById(id: Product['id']) {
+  try {
+    const url = `${import.meta.env.VITE_API_URL}/api/products/${id}`;
+    const { data } = await axios(url);
+    const result = safeParse(ProductSchema, data.data);
+    if (result.success) {
+      return result.output;
+    } else {
+      throw new Error('Hubo un error... :(');
     }
   } catch (error) {
     console.log(error);
